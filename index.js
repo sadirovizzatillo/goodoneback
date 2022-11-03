b3cfec4dc2165bc5563b12f5eed065543f0bf723
sadirov4647 <sadirovizzatillo3@gmail.com>
2022-11-03 16:04:42 +0500

    s

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ if (!config.get('jwtPrivateKey')) {
   process.exit(1);
 }
 
-mongoose.connect("mongodb+srv://Sadirov:a-z123456789@cluster0.csxvtdd.mongodb.net/test?retryWrites=true&w=majority", {  
-useNewUrlParser: true, 
-useUnifiedTopology: true  }).then(() => {
-  console.log("mongo db ga ulandi")
+mongoose.connect("mongodb+srv://Sadirov:a-z123456789@cluster0.csxvtdd.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }).then(() => {
+console.log("mongo db ga ulandi")
 }).catch((err) => {
   console.error("mongoDb ga ulanish xato", err)
 })
@@ -38,6 +36,8 @@ app.use("/auth", authRoute)
 
 
 const port = process.env.Port || 5000;
+console.log(process.env.Port)
+
 app.listen(port, () => {
   console.log(`${port} portga ulandi`)
 })
